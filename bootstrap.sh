7ad623e8ef16db3786a64c4b491d4ca95b737f39
Jonas Bork <59069012+jonas-bork@users.noreply.github.com>
2025-09-22 14:41:01 +0200

    Updated bootstrap script to properly handle relative files in the services (#8)
    * Updated bootstrap script to properly handle relative files
    
    * Fixed typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,25 @@
 kubectl apply -f ./namespace.yaml
 
 # Bootstrap other services
-../gateway/bootstrap.sh
-../encryption/bootstrap.sh
-../monitoring/bootstrap.sh
-../solver-artifact-registry/bootstrap.sh
-../keycloak/bootstrap.sh
+bootstrap() {
+    echo "==== Bootstrapping $1"
+    cur=$(pwd)
+    # Go to the directory
+    cd "$1" || {
+        echo "Failed to change directory to $1, aborting"
+        return 1
+    }
+    ./bootstrap.sh
+    # Go back to original directory
+    cd "$cur" || {
+        echo "Failed to go back to infrastructure directory: Failed to change directory to $cur, aborting"
+        return 1
+    }
+}
 
+bootstrap ../gateway/
+bootstrap ../encryption/
+bootstrap ../monitoring/
+bootstrap ../solver-artifact-registry/
+bootstrap ../keycloak/
 
