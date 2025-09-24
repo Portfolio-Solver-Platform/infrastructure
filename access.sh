60539d6779edfef46f0e4035c73fcd27a4ec6fa6
Sofus Laubel <sofus@laubel.net>
2025-09-24 10:03:33 +0200

    fixed bash path in shell files

--- access.sh
+++ access.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 sh ../gateway/minikube/access.sh
 
--- post-data-setup.sh
+++ post-data-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cd ../solver-artifact-registry && sh apply_terraform.sh
 cd ../minizinc-solvers && sh push_to_harbor.sh
