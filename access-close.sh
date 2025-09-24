1db5e1161f4addca1579739dc767b5f8d1f9c5c4
Sofus Laubel <51532553+sofusl90@users.noreply.github.com>
2025-09-24 15:54:48 +0200

    automation and readme uipdate (#15)

--- access-close.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-../gateway/minikube/kill-tunnel.sh
-
--- post-data-setup.sh
+++ post-data-setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-
 cd ../solver-artifact-registry && sh apply_terraform.sh
 cd ../minizinc-solvers && sh push_to_harbor.sh
