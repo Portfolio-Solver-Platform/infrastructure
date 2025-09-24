2648ae4e6eebc85ade5ae482b9f21c7edc7cba36
Sofus Laubel <sofus@laubel.net>
2025-09-24 13:22:54 +0200

    updated update.sh script to also pull from main branch

--- update.sh
+++ update.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
 update() {
-    echo "Pulling from $1..."
+    echo "=========== Pulling from $1... =========="
     cd "$1" || {
         echo "Failed to change directory to $1, aborting"
         return 1
     }
+
+    echo ""
+    echo "---------- pulling main ----------"
+    echo ""
+    git fetch origin
+    git branch -f main origin/main
+
+    echo ""
+    echo "---------- pulling your current branch ----------"
+    echo ""
     git pull
+
 }
 
 update ../infrastructure/
