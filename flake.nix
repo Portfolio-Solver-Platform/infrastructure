8a425231795e5c5f60bc8befdd2485bffe08e962
Jonas Bork <59069012+jonas-bork@users.noreply.github.com>
2026-03-16 11:52:05 +0100

    Start implementing FluxCD for deployment (#32)
    * Install Flux, and uninstall potentially unused programs
    
    * Init flux
    
    * Infrastructure fix
    
    * Add gateway
    
    * Use the wip-add-dep for gateway
    
    * CRDs
    
    * Add gateway routes
    
    * Routes name fix
    
    * Added monitoring, secrets-manager and artifact-registry namespaces
    
    * Add Keycloak
    
    * Fix Keycloak helm path
    
    * Dev values for Keycloak
    
    * Created base cluster and added tofu-controller
    
    * Added kustomization to base
    
    * Add Keycloak Terraform setup
    
    * Updated keycloak url
    
    * Fix indent
    
    * Replaced dependsOn with retryInterval
    
    * Use wip-flux branch of auth-manager
    
    * Added message-broker namespace
    
    * Add tf-runner role
    
    * Add artifact-registry (Harbor)
    
    * Set Harbor admin password
    
    * Set Harbor url
    
    * Fix Harbor
    
    * Add drift detection to routes
    
    * Name the artifact registry helm release "harbor"
    
    * Stop Terraform for Harbor
    
    * Drift detection
    
    * Remove Terraform patch
    
    * Revision reconcile strategy
    
    * Add Harbor terraform
    
    * Introduce apps
    
    * Add apps to kustomization
    
    * Create dev profile in apps
    
    * Update user service name
    
    * chore(user): update user image to main-bcec3903-20260316103230
    
    * Added some tools back to the nix environment
    
    * Update flake inputs
    
    * Rename user-service.yaml to user.yaml
    
    ---------
    
    Co-authored-by: infrastructure-bot[bot] <infrastructure-bot[bot]@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -8,6 +9,7 @@
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -16,6 +18,10 @@
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
+        };
+
+        pkgsUnstable = import nixpkgs-unstable {
+          inherit system;
         };
       in
       {
@@ -29,16 +35,19 @@
             '';
 
           packages = with pkgs; [
+            pkgsUnstable.fluxcd
             minikube
             docker
             bash
-            terraform
-            cosign
-            skaffold
-            kubernetes-helm
             git
             kubectl
             kustomize
+            kubernetes-helm
+            terraform
+
+            # TODO: Remove the following, only used for development of other services
+            cosign
+            skaffold
             (python3.withPackages (
               ps: with ps; [
                 requests
