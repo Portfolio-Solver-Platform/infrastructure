3e1ee7f8171df0b6c97a240de3dd90b6a6a00f0b
Jonas Bork <jonas10bork@gmail.com>
2026-03-24 11:28:00 +0100

    Update CI

--- flake.nix
+++ flake.nix
@@ -25,29 +25,42 @@
         };
       in
       {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            pkgsUnstable.fluxcd
-            minikube
-            docker
-            bash
-            git
-            kubectl
-            kustomize
-            kubeconform
-            kubernetes-helm
-            terraform
-            (python3.withPackages (
-              ps: with ps; [
-                requests
-              ]
-            ))
+        devShells =
+          let
+            ciPackages = with pkgs; [
+              pkgsUnstable.fluxcd
+              kubeconform
+              bash
+            ];
+          in
+          {
+            ci = pkgs.mkShell {
+              packages = ciPackages;
+            };
 
-            # The following are only used for development of other services
-            cosign
-            skaffold
-          ];
-        };
+            default = pkgs.mkShell {
+              packages =
+                ciPackages
+                ++ (with pkgs; [
+                  minikube
+                  docker
+                  git
+                  kubectl
+                  kustomize
+                  kubernetes-helm
+                  terraform
+                  (python3.withPackages (
+                    ps: with ps; [
+                      requests
+                    ]
+                  ))
+
+                  # The following are only used for development of other services
+                  cosign
+                  skaffold
+                ]);
+            };
+          };
       }
     );
 }
