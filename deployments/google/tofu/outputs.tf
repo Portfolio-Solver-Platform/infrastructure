c5f763187904940bea6da7778eb30a3279776a23
Jonas Bork <jonas10bork@gmail.com>
2026-04-12 15:42:46 +0200

    Assign an IP to the transit secrets manager

--- deployments/google/tofu/outputs.tf
+++ deployments/google/tofu/outputs.tf
@@ -7,3 +7,8 @@ output "psp_cluster_zone" {
   value       = google_container_cluster.psp.location
   description = "The zone of the psp cluster"
 }
+
+output "secrets_manager_transit_ip" {
+  description = "The static internal IP address of the transit secrets manager VM"
+  value       = google_compute_address.secrets_manager_transit_internal_ip.address
+}
